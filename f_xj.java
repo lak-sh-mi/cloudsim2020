5e181cebc9591e76a4c53e58cc3d9b3a27f9f056
lak-sh-mi <56106475+lak-sh-mi@users.noreply.github.com>
2020-12-25 11:42:36 +0530

    Add files via upload

---
+++ f_xj.java
@@ -0,0 +1,8 @@
+package Package1;
+abstract class f_xj  
+{
+  // single function  multi independent variable
+  // a single value is returned indiced to equation_ref
+
+  abstract double func(double x[]);
+}
